0c6f546a614ebc0e9309bee886ef4c25fe85a1db
João Roberto de Oliveira Rêgo <joaocte@gmail.com>
2021-11-19 18:38:14 -0300

    Removendo o Id do comando de atualizar.

--- Jr.Backend.Fornecedores.Domain/Commands/Request/AtualizarFornecedorCommand.cs
+++ Jr.Backend.Fornecedores.Domain/Commands/Request/AtualizarFornecedorCommand.cs
@@ -2,11 +2,13 @@
 using MediatR;
 using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Jr.Backend.Fornecedores.Domain.Commands.Request
 {
     public class AtualizarFornecedorCommand : FornecedorCommandRequest, IRequest<AtualizarFornecedorCommandResponse>
     {
+        [JsonIgnore]
         public Guid Id { get; }
 
         public AtualizarFornecedorCommand(string celular, string cnpj, ICollection<string> emailContato, ICollection<string> emailFatura, InformacoesBancariasRequest informacoesBancarias, string nomeContato, Guid id) : base(celular, cnpj, emailContato, emailFatura, informacoesBancarias, nomeContato)
